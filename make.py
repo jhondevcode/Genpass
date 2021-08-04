830b315290c50c61acdd3b026b49fe80dbdb79f7
jhondev-code <jhondev.code@gmail.com>
2021-08-04 17:48:31 -0500

    python makefile has been added

---
+++ make.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import subprocess as sp
+
+
+EXECUTABLE = "genpass.py"
+PATH = os.path.join(os.getcwd())
+VENV = "/bin/activate"
+
+
+def activate():
+    sp.call(["source", f"{PATH}/{VENV}"])
+    # os.system(f"source {PATH}/{VENV}")
+
+
+def clean():
+    sp.call(["rm", "-r", "build"])
+    sp.call(["rm", "-r", "dist"])
+    sp.call(["rm", "-r", "__pycache__"])
+    sp.call(["rm", "genpass.spec"])
+
+
+def compile():
+    sp.call(["pyinstaller", "-F", "-noconsole", EXECUTABLE])
+
+
+def main(args: list):
+    if len(args) > 0:
+        if args[0] == "compile":
+            compile()
+        elif args[0] == "activate":
+            activate()
+        elif args[0] == "clean":
+            clean()
+        else:
+            print("Task not found :(")
+    else:
+        print("Error:")
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    del args[0]
+    main(args)
